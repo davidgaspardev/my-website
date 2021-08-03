324fb5ad951f811ea9a3457f2ba43ec0bfa56fd9
David Gaspar <davidgaspar.dev@gmail.com>
2021-08-03 15:06:25 -0300

    Adapted to TypeScript

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next"
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse): void {
+
+  res.status(200).json({
+    name: 'David Gaspar',
+    email: 'davidgaspar.dev@gmail.com',
+    career: 'Software Engineer'
+  });
 }
