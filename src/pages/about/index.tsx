8549e85bac84a1ef59ab5985b55c21e7f7fd4dce
David Gaspar <davidgaspar.dev@gmail.com>
2021-12-05 14:46:52 -0300

    Create about empty page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header";
+
+export default function AboutPage(): JSX.Element {
+
+    return (
+        <main>
+            <Header />
+        </main>
+    );
+}
