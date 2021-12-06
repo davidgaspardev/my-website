29bed893c2c0e9f825c9e4173dd6bed6ff37c54b
David Gaspar <davidgaspar.dev@gmail.com>
2021-12-05 19:51:38 -0500

    Style loading issue fixed #1

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 /**
  * Document
@@ -6,11 +7,31 @@ import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/do
 export default class MyDocument extends Document {
 
     static async getInitialProps(context: DocumentContext) {
-        const initialProps = await super.getInitialProps(context);
-
-
-        return { ...initialProps };
-    }
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = context.renderPage;
+    
+        try {
+          context.renderPage = () =>
+            originalRenderPage({
+              enhanceApp: (App) => (props) =>
+                sheet.collectStyles(<App {...props} />),
+            })
+    
+          const initialProps = await Document.getInitialProps(context)
+          return {
+            ...initialProps,
+            styles: (
+              <>
+                {initialProps.styles}
+                {sheet.getStyleElement()}
+              </>
+            ),
+          }
+        } finally {
+          sheet.seal()
+        }
+      }
+    
 
     render(): JSX.Element {
         return (
