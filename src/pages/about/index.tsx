365c4f079eec29848708d5fc0a11fb8d000cf8e4
David Gaspar <davidgaspar.dev@gmail.com>
2021-12-09 18:39:04 -0500

    Building AboutPage

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,38 @@
+import styled from "styled-components";
 import Header from "../../components/Header";
+const aboutMe = `
+Irei falar um pouco sobre mim, quando eu tinha 15 anos conheci um pessoa, nós estudavamos junto na Enseada do Brito em Palhoça e acabos virado amigos, ele me mostrou um aplicativo para celular que ele
+estava desenvolvendo, não lembro os destalhes mas era uma lista TODO, mas sei que fiquei fascinado por aquilo, algo tão simples para mim hoje em dia, mas naquela época era como se fosse magia.
+`;
 
 export default function AboutPage(): JSX.Element {
 
     return (
-        <main>
+        <AboutPageStyled>
             <Header />
-        </main>
+            <div className="about-image">
+
+            </div>
+            <div className="about-content">
+                <h1>Olá, eu sou David Gaspar</h1>
+                <p>{aboutMe}</p>
+            </div>
+        </AboutPageStyled>
     );
 }
+
+const AboutPageStyled = styled.main`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+
+    .about-image {
+        width: 300px;
+    }
+
+    .about-content {
+        width: 500px;
+    }
+`;
