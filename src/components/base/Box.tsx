6924501f1266ea60e02f9f584faab7d633720002
David Gaspar <davidgaspar.dev@gmail.com>
2022-01-13 06:59:08 -0500

    Typography properties removed (Box)

--- src/components/base/Box.tsx
+++ src/components/base/Box.tsx
@@ -1,10 +1,9 @@
 import styled from 'styled-components'
-import { typography, space, color, layout, TypographyProps, SpaceProps, ColorProps, LayoutProps } from 'styled-system'
+import { space, color, layout, SpaceProps, ColorProps, LayoutProps } from 'styled-system'
 
-type BoxProps = TypographyProps & SpaceProps & ColorProps & LayoutProps;
+type BoxProps = SpaceProps & ColorProps & LayoutProps;
 
 export const Box = styled('div')<BoxProps>(
-  typography,
   space,
   color,
   layout
