832fe976cd4f1458e861e47c9c0cbf6cc8c0bc0c
David Gaspar <davidgaspar.dev@gmail.com>
2022-06-14 13:13:59 -0300

    FloatingHeader component added in the about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Header from "../../components/Header";
+import FloatingHeader from "../../components/FloatingHeader";
 const aboutMe = `
 Irei falar um pouco sobre mim, quando eu tinha 15 anos conheci um pessoa, nós estudavamos junto na Enseada do Brito em Palhoça e acabos virado amigos, ele me mostrou um aplicativo para celular que ele
 estava desenvolvendo, não lembro os destalhes mas era uma lista TODO, mas sei que fiquei fascinado por aquilo, algo tão simples para mim hoje em dia, mas naquela época era como se fosse magia.
@@ -9,7 +9,9 @@ export default function AboutPage(): JSX.Element {
 
     return (
         <AboutPageStyled>
-            <Header />
+
+            <FloatingHeader />
+
             <div className="about-image">
 
             </div>
