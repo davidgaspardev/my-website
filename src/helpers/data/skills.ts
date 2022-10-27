6d2b8f5bf252d9c3af064533b40afe95b5c93283
David Gaspar <davidgaspar.dev@gmail.com>
2022-10-26 21:24:14 -0300

    chore: add Android and Git skills

--- src/helpers/data/skills.ts
+++ src/helpers/data/skills.ts
@@ -1,39 +1,39 @@
 import { SkillInfo } from "../types";
 
 export const skills: SkillInfo[] = [
-    {
-        iconPath: "/static/images/svg/icon-flutter.svg",
-        name: "Flutter",
-        description: "I use it for mobile app development"
-    },
-    {
-        iconPath: "/static/images/svg/icon-golang.svg",
-        name: "Golang",
-        description: "I use it for microservices development"
-    },
-    {
-        iconPath: "/static/images/svg/icon-react.svg",
-        name: "React.JS",
-        description: "Consigo desensolver qualuqer interface com o Flutter."
-    },
-    {
-        iconPath: "/static/images/svg/icon-nodejs.svg",
-        name: "Node.JS",
-        description: "Consigo desensolver qualuqer interface com o Flutter."
-    },
-    {
-        iconPath: "/static/images/svg/icon-figma.svg",
-        name: "Figma",
-        description: "Consigo desensolver qualuqer interface com o Flutter."
-    },
-    {
-        iconPath: "/static/images/svg/icon-flutter.svg",
-        name: "Android",
-        description: "Consigo desensolver qualuqer interface com o Flutter."
-    },
-    {
-        iconPath: "/static/images/svg/icon-flutter.svg",
-        name: "Git",
-        description: "Consigo desensolver qualuqer interface com o Flutter."
-    }
-]; 
+	{
+		iconPath: "/static/images/svg/icon-flutter.svg",
+		name: "Flutter",
+		description: "I use it for mobile app development",
+	},
+	{
+		iconPath: "/static/images/svg/icon-golang.svg",
+		name: "Golang",
+		description: "I use it for microservices development",
+	},
+	{
+		iconPath: "/static/images/svg/icon-react.svg",
+		name: "React.JS",
+		description: "Consigo desensolver qualuqer interface com o Flutter.",
+	},
+	{
+		iconPath: "/static/images/svg/icon-nodejs.svg",
+		name: "Node.JS",
+		description: "Consigo desensolver qualuqer interface com o Flutter.",
+	},
+	{
+		iconPath: "/static/images/svg/icon-figma.svg",
+		name: "Figma",
+		description: "Consigo desensolver qualuqer interface com o Flutter.",
+	},
+	{
+		iconPath: "/static/images/svg/icon-android.svg",
+		name: "Android",
+		description: "Consigo desensolver qualuqer interface com o Flutter.",
+	},
+	{
+		iconPath: "/static/images/svg/icon-git.svg",
+		name: "Git",
+		description: "Consigo desensolver qualuqer interface com o Flutter.",
+	},
+];
