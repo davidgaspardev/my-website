cb1430d610a89ffc4680b79ab59474c71fc27699
David Gaspar <davidgaspar.dev@gmail.com>
2022-11-23 14:29:22 -0300

    chore: add indentation

--- src/components/TagGithub.tsx
+++ src/components/TagGithub.tsx
@@ -3,46 +3,48 @@ import Image from "next/image";
 import styled from "styled-components";
 
 type Props = {
-    href: string;
+	href: string;
 };
 
 /**
  * Tag Github
- * 
+ *
  * @returns {JSX.Element}
  */
 export default function TagGithub(props: Props): JSX.Element {
-    const { href } = props;
+	const { href } = props;
 
-    return (
-        <TagGithubContainer>
-            <Link href={href}>
-                <Image
-                    src="/static/images/svg/icon-github-fixed.svg"
-                    width={75}
-                    height={75}/>
-            </Link>
-        </TagGithubContainer>
-    );
+	return (
+		<TagGithubContainer>
+			<Link href={href}>
+				<Image
+					src="/static/images/svg/icon-github-fixed.svg"
+					width={75}
+					height={75}
+					alt="Github logo"
+				/>
+			</Link>
+		</TagGithubContainer>
+	);
 }
 
 const TagGithubContainer = styled.span`
-    position: fixed;
-    top: auto;
-    right: 0;
-    bottom: -5px;
-    transform: rotate(90deg);
-    z-index: 100;
+	position: fixed;
+	top: auto;
+	right: 0;
+	bottom: -5px;
+	transform: rotate(90deg);
+	z-index: 100;
 
-    @media(min-width: 1100px) {
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: auto;
-        transform: rotate(0deg);
-    }
+	@media (min-width: 1100px) {
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: auto;
+		transform: rotate(0deg);
+	}
 
-    img {
-        cursor: pointer;
-    }
+	img {
+		cursor: pointer;
+	}
 `;
