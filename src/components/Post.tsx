f5e7ac04fa07a3adbb2c41780e19f25d6d72c89f
David Gaspar <davidgaspar.dev@gmail.com>
2022-12-31 23:28:28 -0300

    refactor: migrate from styled-compnent to stitches

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,95 +1,136 @@
 import { useRef } from "react";
-import styled from "styled-components";
+import { styled } from ".";
+// import styled from "styled-components";
 import { PostMetadata } from "../helpers/types";
-import { Box } from "./base/Box";
-import { Flex } from "./base/Flex";
-import { Paragraph } from "./base/Paragraph";
-import { Text } from "./base/Text";
+import Image from "next/image";
+import { format } from "date-fns";
 
 type Props = {
-    onClick: () => void;
-    data: PostMetadata;
+	onClick: () => void;
+	data: PostMetadata;
 };
 
 export default function Post(props: Props): JSX.Element {
-    const { data, onClick } = props;
-    const dateRef = useRef<Date>(new Date(data.date));
+	const { data, onClick } = props;
+	const dateRef = useRef<Date>(new Date(data.date));
 
-    return (
-        <Box
-            onClick={onClick}
-            maxWidth={600}
-            width="100%"
-            m={20}>
-            <Container>
-
-                <Flex flexDirection="row" justifyContent="space-between" alignItems="flex-start">
-                    <h3>{data.title}</h3>
-
-                    <Paragraph textAlign="end" fontSize="8pt">
-                        {`${dateRef.current.getDate()}/${dateRef.current.getMonth()}/${dateRef.current.getFullYear()}`}
-                    </Paragraph>
-                </Flex>
-
-                <Text className="excerpt">{data.excerpt}</Text>
-                
-                <Flex flexDirection="row-reverse">
-                    {
-                        data.labels && data.labels.map((label, index) => <SpanLabel key={index}>{label}</SpanLabel>)
-                    }
-                </Flex>
-
-            </Container>
-        </Box>
-    );
+	return (
+		<PostWrapper>
+			<PostContainer onClick={onClick}>
+				<PostDate>
+					<p>{format(dateRef.current, "dd/MM/yyyy")}</p>
+				</PostDate>
+				<PostTitle>
+					<h1>{data.title}</h1>
+				</PostTitle>
+				<PostDescrition>
+					<p>{data.excerpt}</p>
+				</PostDescrition>
+				<PostAuthor>
+					<Image
+						src={data.author.picture}
+						width={32}
+						height={32}
+						alt="Author picture"
+					/>
+					<h4>{data.author.name}</h4>
+				</PostAuthor>
+				<PostTags>
+					{data.labels?.map((label, index) => (
+						<PostTag key={index}>{label}</PostTag>
+					))}
+				</PostTags>
+			</PostContainer>
+		</PostWrapper>
+	);
 }
 
-const Container = styled.div`
-    padding: 20px;
-    border-radius: 10px;
-    cursor: pointer;
-    transition: all ease 500ms;
-    border: 1px solid transparent;
+const PostWrapper = styled("div", {
+	maxWidth: 750,
+	width: "100%",
+	padding: "$4",
+});
 
-    &:hover {
-        background-color: rgba(128,128,128,.05);
-    }
+const PostContainer = styled("div", {
+	display: "grid",
+	gridTemplateAreas: `
+		"date date date date"
+		"title title title title"
+		"description description description description"
+		"author tags tags tags"
+	`,
+	gridTemplateColumns: "1fr 1fr 1fr 1fr",
+	gap: "$2",
 
-    h3 {
-        padding-bottom: 10px;
-        font-family: 'Readex Pro', sans-serif;
-        font-size: 16pt;
-    }
+	"@media (max-width: 600px)": {
+		gridTemplateAreas: `
+			"author author date date"
+			"title title title title"
+			"description description description description"
+			"tags tags tags tags"
+		`,
+	},
+});
 
-    p {
-        opacity: 0.75;
-        font-family: 'IBM Plex Sans', sans-serif;
-    }
+const PostDate = styled("div", {
+	gridArea: "date",
 
-    .excerpt {
-        margin: 10px 0px;
-    }
-`;
+	p: {
+		opacity: 0.8,
+		fontSize: "$2",
+	},
 
-function SpanLabel(props: { children: string }) {
-    const { children } = props;
+	"@media (max-width: 600px)": {
+		textAlign: "end",
+	},
+});
 
-    return (
-        <SpanLabelContainer>
-            <Text fontSize="10pt">{ children.toLocaleLowerCase() }</Text>
-        </SpanLabelContainer>
-    );
-}
+const PostTitle = styled("div", {
+	gridArea: "title",
+	color: "$greenDark600",
+});
 
-const SpanLabelContainer = styled.div`
-    background-color: #83c6c1;
-    border-radius: 5px;
-    margin-left: 10px;
-    padding: 5px 8px;
+const PostDescrition = styled("div", {
+	gridArea: "description",
+});
 
-    p {
-        color: white;
-        padding: 0;
-        margin: 0;
-    }
-`;
+const PostAuthor = styled("div", {
+	gridArea: "author",
+
+	display: "flex",
+	flexDirection: "row",
+	alignItems: "center",
+
+	h4: {
+		fontWeight: "600",
+		color: "$greenDark600",
+	},
+
+	img: {
+		borderRadius: 16,
+		marginRight: "$2",
+	},
+
+	"@media (max-width: 600px)": {
+		img: {
+			display: "none",
+		},
+	},
+});
+
+const PostTags = styled("div", {
+	gridArea: "tags",
+
+	display: "flex",
+	flexDirection: "row-reverse",
+	alignItems: "center",
+});
+
+const PostTag = styled("span", {
+	padding: "$1",
+	borderRadius: "$1",
+	mx: "$1",
+	backgroundColor: "#00403a50",
+	color: "$green600",
+	fontFamily: "Readex Pro",
+});
