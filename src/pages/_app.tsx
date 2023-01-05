bf65828d529bff448f37e861acca92a5de3fa05c
David Gaspar <davidgaspar.dev@gmail.com>
2023-01-05 01:23:54 -0300

    chore: add Open Graph protocol

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,18 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 		<>
 			<Head>
 				<title>David Gaspar</title>
+
+				<meta property="og:url" content="https://davidgaspar.dev" />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="David Gaspar Dev" />
+				<meta
+					property="og:description"
+					content="My website for you to know more about me"
+				/>
+				<meta
+					property="og:image"
+					content="https://firebasestorage.googleapis.com/v0/b/myself-dg.appspot.com/o/my-website%2Fthumb.png?alt=media&token=b3414b3a-7d44-4d36-984c-f5079e17f5a6"
+				/>
 			</Head>
 			<Component {...pageProps} />
 		</>
