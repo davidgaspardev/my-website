ee1849ab1787d51df9e68c906d4a585d40c532cd
David Gaspar <davidgaspar.dev@gmail.com>
2023-01-04 23:57:36 -0300

    refactor: delete components unused

--- src/components/base/Text.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import { typography, TypographyProps } from 'styled-system';
-
-type TextProps = TypographyProps;
-
-export const Text = styled('p')<TextProps>(typography);
