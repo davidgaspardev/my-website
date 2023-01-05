462f72103595b4e2edff28a0d427204c9eb1bff3
David Gaspar <davidgaspar.dev@gmail.com>
2023-01-05 01:37:03 -0300

    chore: add Open Graph protocol for Twitter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,6 +20,20 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 					property="og:image"
 					content="https://firebasestorage.googleapis.com/v0/b/myself-dg.appspot.com/o/my-website%2Fthumb.png?alt=media&token=b3414b3a-7d44-4d36-984c-f5079e17f5a6"
 				/>
+
+				{/* Twitter Meta Tags */}
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta property="twitter:domain" content="davidgaspar.dev" />
+				<meta property="twitter:url" content="https://davidgaspar.dev" />
+				<meta name="twitter:title" content="David Gaspar Dev" />
+				<meta
+					name="twitter:description"
+					content="My website for you to know more about me"
+				/>
+				<meta
+					name="twitter:image"
+					content="https://firebasestorage.googleapis.com/v0/b/myself-dg.appspot.com/o/my-website%2Fthumb.png?alt=media&token=b3414b3a-7d44-4d36-984c-f5079e17f5a6"
+				/>
 			</Head>
 			<Component {...pageProps} />
 		</>
