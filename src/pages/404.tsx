4850dd5fcea2c5df14115f9d2a8f5c468d3d4b8d
David Gaspar <davidgaspar.dev@gmail.com>
2023-01-04 23:56:59 -0300

    refactor: update 404 page to use stitches

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,69 +1,70 @@
 import Image from "next/image";
 import Link from "next/link";
-import styled from "styled-components";
+import { styled } from "../components";
+import { Flex } from "../components/base/Flex";
 
 export default function NotFound() {
 	return (
-		<NotFoundStyled>
-			<div className="logo-panic">
+		<NotFoundStyled
+			flexDirection="row"
+			flexWrap="wrap"
+			alignItems="center"
+			alignContent="center"
+			justifyContent="center"
+		>
+			<LogoPanic>
 				<Image
 					src="/static/images/svg/logo-panic.svg"
 					width={350}
 					height={350}
 					alt="Panic logo"
 				/>
-			</div>
+			</LogoPanic>
 
-			<div className="not-found-content">
+			<NotFoundContent flexDirection="column">
 				<h1>404 Not Found</h1>
-				<h3>Desculpa mas ainda não desenvolvi essa parte.</h3>
+				<h3>Desculpa mas ainda não desenvolvi essa parte...</h3>
 
-				<Link href="/">volta para home</Link>
-			</div>
+				<Link href="/">ir para home</Link>
+			</NotFoundContent>
 		</NotFoundStyled>
 	);
 }
 
-const NotFoundStyled = styled.div`
-	width: 100%;
-	height: 100vh;
-	display: flex;
-	flex-direction: row;
-	flex-wrap: wrap;
-	align-items: center;
-	align-content: center;
-	justify-content: center;
+const NotFoundStyled = styled(Flex, {
+	width: "100%",
+	height: "100vh",
+});
 
-	.logo-panic {
-		width: 350px;
-	}
+const LogoPanic = styled("div", {
+	maxWidth: 350,
+	width: "100%",
+});
 
-	.not-found-content {
-		display: flex;
-		flex-direction: column;
-	}
+const NotFoundContent = styled(Flex, {
+	padding: "$1",
 
-	h1,
-	h3 {
-		font-family: "Hind Siliguri", san-serif;
-	}
+	"h1,h3": {
+		fontFamily: "Hind Siliguri",
+	},
 
-	h1 {
-		color: var(--color-primary);
-	}
+	h1: {
+		color: "$green100",
+	},
 
-	h3 {
-		color: var(--color-secondary);
-		font-weight: normal;
-	}
+	h3: {
+		color: "$green400",
+		fontWeight: "normal",
+		opacity: 0.7,
+	},
 
-	a {
-		align-self: flex-end;
-		padding: 8px;
-		margin-top: 8px;
-		background-color: #f7f7f7;
-		border-radius: 5px;
-		border: 1px solid var(--color-secondary);
-		color: var(--color-primary);
-	}
-`;
+	a: {
+		alignSelf: "flex-end",
+		padding: "$1 $2",
+		marginTop: "$2",
+		backgroundColor: "$green200",
+		borderRadius: "$1",
+		color: "white",
+		textTransform: "uppercase",
+	},
+});
