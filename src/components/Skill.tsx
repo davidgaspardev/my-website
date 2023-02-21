dd6512843ec32b953a9c6f183b4e4ad661bb473f
David Gaspar <davidgaspar.dev@gmail.com>
2023-02-21 17:20:46 -0300

    chore: add modal to show Skill description

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,7 +1,8 @@
 import { SkillInfo } from "../helpers/types";
 import Image from "next/image";
 import { styled } from ".";
-import { useEffect, useId, useRef } from "react";
+import { useEffect, useId, useRef, useState } from "react";
+import { hideScrollbar, showScrollbar } from "../utils/scrollbar";
 
 type Props = {
 	data: SkillInfo;
@@ -13,6 +14,9 @@ export default function Skill(props: Props) {
 
 	const id = useId();
 	const hoverRef = useRef<boolean>(false);
+
+	const [isDescriptionVisible, setDescriptionVisible] =
+		useState<boolean>(false);
 
 	useEffect(() => {
 		const card = document.getElementById(id) as HTMLDivElement;
@@ -36,20 +40,34 @@ export default function Skill(props: Props) {
 	}, [id]);
 
 	return (
-		<SkillContainer>
-			<Card id={id}>
-				<CardImage>
-					<Image
-						id={`${id}-image`}
-						src={iconPath}
-						width={75}
-						height={75}
-						alt="Skill logo"
-					/>
-				</CardImage>
-				<h3>{name}</h3>
-			</Card>
-		</SkillContainer>
+		<>
+			<SkillContainer>
+				<Card
+					id={id}
+					onClick={() => {
+						console.log("clique", id);
+						setDescriptionVisible(!isDescriptionVisible);
+					}}
+				>
+					<CardImage>
+						<Image
+							id={`${id}-image`}
+							src={iconPath}
+							width={75}
+							height={75}
+							alt="Skill logo"
+						/>
+					</CardImage>
+					<h3>{name}</h3>
+				</Card>
+			</SkillContainer>
+
+			<DescriptionModal
+				showDescription={isDescriptionVisible}
+				description={description}
+				onClose={() => setDescriptionVisible(false)}
+			/>
+		</>
 	);
 }
 
@@ -91,5 +109,138 @@ const CardImage = styled("div", {
 		transitionDuration: "500ms",
 		transitionPropety: "transform",
 		transitionTimingFunction: "ease",
+	},
+});
+
+function DescriptionModal(props: {
+	showDescription: boolean;
+	description: string;
+	onClose?: () => void;
+}): JSX.Element {
+	const { showDescription, description, onClose } = props;
+	const id = useId();
+
+	function closeModal() {
+		const decriptionModalElement = document.getElementById(id);
+		if (!decriptionModalElement) return;
+
+		decriptionModalElement.style.opacity = "0";
+
+		setTimeout(() => {
+			decriptionModalElement.style.display = "none";
+			showScrollbar();
+			if (onClose) onClose();
+		}, 500);
+	}
+
+	function openModal() {
+		hideScrollbar();
+
+		const decriptionModalElement = document.getElementById(id);
+		if (!decriptionModalElement) return;
+
+		decriptionModalElement.style.display = "flex";
+
+		setTimeout(() => {
+			decriptionModalElement.style.opacity = "1";
+		}, 128);
+	}
+
+	function handleEscKey(event: KeyboardEvent) {
+		if (event.code === "Escape") closeModal();
+	}
+
+	useEffect(() => {
+		console.log("component did mount");
+
+		document.addEventListener("keydown", handleEscKey);
+
+		return () => {
+			document.removeEventListener("keydown", handleEscKey);
+		};
+	}, []);
+
+	useEffect(() => {
+		if (showDescription) openModal();
+	}, [showDescription, id]);
+
+	return (
+		<DescriptionModalContainer id={id}>
+			<DescriptionModalContent>
+				<p>{description}</p>
+			</DescriptionModalContent>
+
+			<DescriptionModalClose onClick={() => closeModal()}>
+				<h4>X</h4>
+			</DescriptionModalClose>
+
+			<DescriptionModalObs>
+				<p>
+					press <strong>ESC</strong> key to close
+				</p>
+			</DescriptionModalObs>
+		</DescriptionModalContainer>
+	);
+}
+
+const DescriptionModalContainer = styled("div", {
+	position: "fixed",
+	top: 0,
+	right: 0,
+	bottom: 0,
+	left: 0,
+
+	display: "none",
+	opacity: 0,
+	alignItems: "center",
+	justifyContent: "center",
+
+	background: "#00000064",
+	zIndex: "$max",
+	transition: "opacity ease 500ms",
+});
+
+const DescriptionModalContent = styled("div", {
+	maxWidth: 320,
+	width: "100%",
+
+	padding: "$5",
+	borderRadius: "$2",
+	borderStyle: "solid",
+	borderWidth: "$1",
+	borderColor: "$green200",
+	backgroundColor: "$green50",
+});
+
+const DescriptionModalClose = styled("div", {
+	position: "absolute",
+	top: "$2",
+	right: "$2",
+
+	cursor: "pointer",
+
+	h4: {
+		color: "white",
+	},
+});
+
+const DescriptionModalObs = styled("div", {
+	position: "absolute",
+	bottom: 0,
+	left: "calc(50% - (300px / 2))",
+
+	maxWidth: 300,
+	width: "100%",
+
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+
+	p: {
+		color: "white",
+		padding: "$1 $2",
+		marginBottom: "$2",
+		borderRadius: "$1",
+		backgroundColor: "#FFFFFF32",
 	},
 });
