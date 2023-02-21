2db3b75e06f83666e2eb96ac87f425daf19df9f4
David Gaspar <davidgaspar.dev@gmail.com>
2023-02-21 17:17:24 -0300

    chore: set description content to skills

--- src/helpers/data/skills.ts
+++ src/helpers/data/skills.ts
@@ -4,36 +4,43 @@ export const skills: SkillInfo[] = [
 	{
 		iconPath: "/static/images/svg/icon-flutter.svg",
 		name: "Flutter",
-		description: "I use it for mobile app development",
+		description:
+			"In Flutter, my focus is on developing frontend for mobile devices. I am able to create beautifully designed screens with completely original animations that are defined by UX/UI principles.",
 	},
 	{
 		iconPath: "/static/images/svg/icon-golang.svg",
 		name: "Golang",
-		description: "I use it for microservices development",
+		description:
+			"In Golang, my focus is on developing microservices, both in HTTP and MQTT scenarios (as well as Modbus). I have already developed many custom packages for specific activities in my work.",
 	},
 	{
 		iconPath: "/static/images/svg/icon-react.svg",
 		name: "React.JS",
-		description: "Consigo desensolver qualuqer interface com o Flutter.",
+		description:
+			"In React.js, my focus is on frontend development for web and desktop applications (for desktop, I use Electron.JS). I have no problem developing new screens from scratch, nor in managing multiple states of components. I always use TypeScript.",
 	},
 	{
 		iconPath: "/static/images/svg/icon-nodejs.svg",
 		name: "Node.JS",
-		description: "Consigo desensolver qualuqer interface com o Flutter.",
+		description:
+			"In Node.js, my focus is on fast scalable backend development. I try to take full advantage of its simplicity in coding, but always taking care not to compromise the performance of the application. I always use TypeScript.",
 	},
 	{
 		iconPath: "/static/images/svg/icon-figma.svg",
 		name: "Figma",
-		description: "Consigo desensolver qualuqer interface com o Flutter.",
+		description:
+			"In Figma, my focus is on bringing my creativity and imagination to the real world. I have no difficulty expressing myself through this tool and I use it a lot.",
 	},
 	{
 		iconPath: "/static/images/svg/icon-android.svg",
 		name: "Android",
-		description: "Consigo desensolver qualuqer interface com o Flutter.",
+		description:
+			"On Android, my focus is on bringing the performance of native development. I can handle Jetpack very well, and my experience with Flutter has added a lot to Android Compose, so I use Kotlin.",
 	},
 	{
 		iconPath: "/static/images/svg/icon-git.svg",
 		name: "Git",
-		description: "Consigo desensolver qualuqer interface com o Flutter.",
+		description:
+			"In Git, my focus is on leaving my code well fragmented with standardized messages, following conventions. My greatest proficiency with Git is through the CLI, but I have no prejudice in using a GUI.",
 	},
 ];
