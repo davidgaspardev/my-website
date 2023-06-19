93a7961b59577d3814f6a94a16627de0f928664e
David Gaspar <davidgaspar.dev@gmail.com>
2023-06-19 20:50:44 -0300

    update: add parameters to color list to get random

--- src/helpers/utils/random.ts
+++ src/helpers/utils/random.ts
@@ -6,6 +6,9 @@ export function randomByte(): number {
   return Math.floor(Math.random() * 256);
 }
 
-export function randomColor(): string {
+export function randomColor(...colors: string[]): string {
+  if (colors.length > 1) {
+    return colors[randomIntBetween(0, colors.length - 1)];
+  }
   return `rgb(${randomByte()},${randomByte()},${randomByte()})`;
 }
